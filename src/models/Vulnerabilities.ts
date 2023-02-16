46fb1d95cabba85d8970c13d35204af7d4a4fda1
rodrigo zanardo <rzanardo1@gmail.com>
2023-02-16 16:39:03 -0300

    vulnerabilidade

---
+++ src/models/Vulnerabilities.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+
+const VulnerabilitiesSchema = new Schema(
+  {
+    code: {
+      type: String,
+      require: true
+    },
+    description: {
+      type: String,
+      require: true
+    },
+    status: {
+      type: Boolean,
+      require: true,
+      default: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+ const mongose = mongoose.model('vulnerabilities', VulnerabilitiesSchema);
+
+ export class vulnerabilities {}
